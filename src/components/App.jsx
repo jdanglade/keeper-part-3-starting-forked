989587c7b46c4870c5d5eab29625c57c7f9e60e5
Justin Danglade <just.danglade@gmail.com>
2020-10-17 11:48:35 -0400

    Still working on CreateArea

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      <div>
+        <ul>
+          {notes.map((noteItem, index) => (
+            <Note
+              key={index}
+              id={index}
+              title="Note title"
+              content="Note content"
+              onDeleteNote={deleteNote}
+            />
+          ))}
+        </ul>
+      </div>
+
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleNote(event) {
+    const newTitle = event.target.value;
+    setNote((prevNotes) => {
+      return [
+        ...prevNotes,
+        {
+          title: newTitle,
+          text: prevNotes.content,
+        },
+      ];
+    });
+  }
+
   return (
     <div>
       <form>
         <input name="title" placeholder="Title" />
         <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <button
+          onClick={() => {
+            props.onAdd(note);
+          }}
+        >
+          Add
+        </button>
       </form>
     </div>
   );
